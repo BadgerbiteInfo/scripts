37e1fe32d06fad70fd5a51ed940747ae169d92be
Thor Validator <t.demis@gmail.com>
2022-09-14 15:03:12 +0300

    Update dependencies_install.sh

--- utils/dependencies_install.sh
+++ utils/dependencies_install.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-utils/main/utils/common.sh)
+
+printCyan "1. Updating packages..." && sleep 1
+sudo apt update
+
+printCyan "2. Installing dependencies..." && sleep 1
+sudo apt install -y make gcc jq curl git lz4 build-essential chrony
+
+printCyan "3. Installing go..." && sleep 1
+if [ ! -f "/usr/local/go/bin/go" ]; then
+  source <(curl -s "https://raw.githubusercontent.com/nodejumper-org/cosmos-utils/main/utils/go_install.sh")
+  source .bash_profile
+fi
+
+echo "$(go version)"
