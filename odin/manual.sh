928c466e4e54b8e7151b2f5ea7751d4e44d3c59b
Thor Validator <t.demis@gmail.com>
2022-09-15 11:59:09 +0300

    Update manual.sh

--- odin/manual.sh
+++ odin/manual.sh
@@ -1,5 +1,6 @@
 # Manual Installation Script
 
+#Cloning Repo and Building Binary
 cd || return
 rm -rf odin-core
 git clone https://github.com/ODIN-PROTOCOL/odin-core.git
@@ -7,18 +8,15 @@ cd odin-core || return
 git fetch --tags
 git checkout v0.6.0
 make all
-# mkdir -p $HOME/go/bin
-# sudo cp $HOME/stride/build/strided $HOME/go/bin
 odind version
-# strided version #v0.3.1
- 
-# strided config chain-id $CHAIN_ID
+
+# initialising Binary and Adjusting Fess and Peers
 strided init $NODE_MONIKER --chain-id $CHAIN_ID
- 
 curl https://raw.githubusercontent.com/ODIN-PROTOCOL/networks/master/mainnets/odin-mainnet-freya/genesis.json > ~/.odin/config/genesis.json
 sha256sum $HOME/.odin/config/genesis.json # d6204cd1e90e74bb29e9e0637010829738fa5765869288aa29a12ed83e2847ea
  
 perl -i -pe 's/^minimum-gas-prices = .+?$/minimum-gas-prices = "0.0125loki"/' ~/.odin/config/app.toml
+
 SEEDS="4529fc24a87ff5ab105970f425ced7a6c79f0b8f@odin-seed-01.mercury-nodes.net:29536,c8ee9f66163f0c1220c586eab1a2a57f6381357f@odin.seed.rhinostake.com:16658"
 PEERS="9d16b1ce74a34b869d69ad5fe34eaca614a36ecd@35.241.238.207:26656,02e905f49e1b869f55ad010979931b542302a9e6@35.241.221.154:26656,4847c79f1601d24d3605278a0183d416a99aa093@34.140.252.7:26656,0165cd0d60549a37abb00b6acc8227a54609c648@34.79.179.216:26656"
 sed -i -e 's|^seeds *=.*|seeds = "'$SEEDS'"|; s|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $HOME/.odin/config/config.toml
@@ -29,11 +27,10 @@ sed -i 's|pruning-keep-recent = "0"|pruning-keep-recent = "100"|g' $HOME/.odin/c
 sed -i 's|pruning-interval = "0"|pruning-interval = "10"|g' $HOME/.odin/config/app.toml
 sed -i 's|^snapshot-interval *=.*|snapshot-interval = 0|g' $HOME/.odin/config/app.toml
  
-printCyan "5. Starting service and synchronization..." && sleep 1
- 
+#Setting Up Service
 sudo tee /etc/systemd/system/odind.service > /dev/null << EOF
 [Unit]
-Description=Stride Node
+Description=Odin Node
 After=network-online.target
 [Service]
 User=$USER
@@ -45,31 +42,27 @@ LimitNOFILE=10000
 WantedBy=multi-user.target
 EOF
  
+# Setting Up Statesync
 odind tendermint unsafe-reset-all --home $HOME/.odin/ --keep-addr-book
  
 SNAP_RPC="http://34.79.179.216:26657,http://34.140.252.7:26657,http://35.241.221.154:26657,http://35.241.238.207:26657"
 RPC_ADDR="http://34.79.179.216:26657"
 INTERVAL=2000
- 
 LATEST_HEIGHT=$(curl -s $RPC_ADDR/block | jq -r .result.block.header.height);
 BLOCK_HEIGHT=$(($LATEST_HEIGHT-$INTERVAL))
 TRUST_HASH=$(curl -s "$RPC_ADDR/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
- 
+
 # Displaying Height and hash
 echo "TRUST HEIGHT: $BLOCK_HEIGHT"
 echo "TRUST HASH: $TRUST_HASH"
- 
+
 # editing config.toml with correct values
 sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
 s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC\"| ; \
 s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
 s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ; $HOME/.odin/config/config.toml
  
+# Enabling and Resatrting Service
 sudo systemctl daemon-reload
 sudo systemctl enable odind
 sudo systemctl restart odind
- 
-printLine
-echo -e "Check logs:            ${CYAN}sudo journalctl -u $BINARY -f --no-hostname -o cat ${NC}"
-echo -e "Check synchronization: ${CYAN}$BINARY status 2>&1 | jq .SyncInfo.catching_up${NC}"
-echo -e "More commands:         ${CYAN}$CHEAT_SHEET${NC}"
