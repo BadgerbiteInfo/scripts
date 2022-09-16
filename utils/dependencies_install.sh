69d3c000440cdde3ff29b67b2f46ff11d6bc705c
Thor Validator <t.demis@gmail.com>
2022-09-16 12:11:19 +0300

    Update dependencies_install.sh

--- utils/dependencies_install.sh
+++ utils/dependencies_install.sh
@@ -2,13 +2,14 @@
 LOG_FILE="install.log"
 LOG_PATH=$HOME_FOLDER/$LOG_FILE
 
+
 source <(curl -s https://raw.githubusercontent.com/nodejumper-org/cosmos-utils/main/utils/common.sh)
 
 printCyan "1. Updating packages..." && sleep 1
-sudo apt update
+sudo apt update  >> "$LOG_PATH" 2>&1
 
 printCyan "2. Installing dependencies..." && sleep 1
-sudo apt install -y make gcc jq curl git lz4 build-essential chrony
+sudo apt install -y make gcc jq curl git lz4 build-essential chrony  >> "$LOG_PATH" 2>&1
 
 printCyan "3. Installing go..." && sleep 1
 if [ ! -f "/usr/local/go/bin/go" ]; then
